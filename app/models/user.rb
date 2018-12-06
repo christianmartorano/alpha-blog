791976f1b99c0383aba5e56d4d4fb858faf4480b
Christian Santos Martorano <christiansmartorano@gmail.com>
2018-12-06 17:42:59 +0000

    Adding password digest Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_many :articles
   before_save { self.email = email.downcase }
-  validates :name, presence: true, uniqueness: { case_sensitive: false  }, length: { minimum: 3, maximum: 25 }
+  validates :username, presence: true, uniqueness: { case_sensitive: false  }, length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
             uniqueness: { case_sensitive: false }, length: { maximum: 105 }
+  has_secure_password            
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_184808) do
+ActiveRecord::Schema.define(version: 2018_12_06_152614) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,10 +22,11 @@ ActiveRecord::Schema.define(version: 2018_12_05_184808) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
