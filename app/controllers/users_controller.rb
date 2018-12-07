f55bd4548c219d60ab2fc0fcf66032cb9c2aedaf
Christian Santos Martorano <christiansmartorano@gmail.com>
2018-12-07 11:24:41 +0000

    Show view user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
@@ -25,6 +26,10 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def gravator_for(user, options = {size: 80})
+    
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    
+    size = options[:size]
+    
+    gravatar_url = "https://api.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    
+    image_tag(gravatar_url, alt: "#{user.username} gravatar", class: "img-circle")
+    
+  end
 end
