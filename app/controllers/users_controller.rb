35f566e96aacfb8f54cff0ba47c499b55984ee29
Christian Santos Martorano <christiansmartorano@gmail.com>
2018-12-07 20:11:05 +0000

    Finishing basic auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to alpha blog #{@user.username}"
+      session[:user_id] = @user.id
       redirect_to articles_path
     else
       render 'new'
