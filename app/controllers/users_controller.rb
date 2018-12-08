d6c60ae89b19a3c5c8e040e4ddabcf1c995d6851
Christian Santos Martorano <christiansmartorano@gmail.com>
2018-12-08 15:51:24 +0000

    Add sigin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     if @user.save
       flash[:success] = "Welcome to alpha blog #{@user.username}"
       session[:user_id] = @user.id
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
